697842d2883c4d3b05392e5f0955f19bfd3da465
Veeresh <veereshlee1821@gmail.com>
2025-08-22 21:57:43 +0530

    Added render links

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './login.css'
+import './Login.css'
 import { Link, useNavigate } from 'react-router-dom';
 import { hideMsg, showMessage, showMsg } from '../redux/redux';
 import { useDispatch } from 'react-redux';
