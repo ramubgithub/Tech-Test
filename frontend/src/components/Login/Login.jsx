28ced9019f05aaaca2f029ce7f3478f964289543
veeresh1821 <veereshlee1821@gmail.com>
2025-11-07 08:17:34 +0530

    Final commit

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Login.css'
 import { Link, useNavigate } from 'react-router-dom';
 import { hideMsg, showMessage, showMsg } from '../redux/redux';
